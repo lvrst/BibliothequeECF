ccf5865656dd21528f63e39c28780cae61f339d7
lvrst <leo.verset@gmail.com>
2021-07-15 14:01:19 +0200

    Ajout des schémas de bases de données

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,44 @@ class TestController extends AbstractController
     /**
      * @Route("/test", name="test")
      */
-    public function index(): Response
+    public function index(
+        UserRepository $userRepository,
+        LivreRepository $livreRepository): Response
     {
+        // Récupération de l'entity manager.
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // Utilisateurs
+        // Requêtes de lecture 
+        // Récupération de la liste complète des users.
+        $users = $userRepository->findAll();
+        dump($users);
+
+        // Récupération du user dont l'id est 1.
+        $user = $userRepository->find(1);
+        dump($user);
+
+        // Récupération du user dont l'email est celui indiqué.
+        $foo = $userRepository->findOneByEmail(`foo.foo@example.com`);
+        dump($foo);
+
+        // Récupération de la liste complète des users qui ont le rôle ROLE_EMPRUNTEUR.
+        $emprunteurRoles = $userRepository->findByRoles('ROLE_EMPRUNTEUR');
+        dump($emprunteurRoles);
+
+        // Livres 
+        // Requêtes de lecture
+        // Récupération de la liste complète des livres.
+        $livres = $livreRepository->findAll();
+        dump($livres);
+
+        // Récupération du livre dont l'id est 1. 
+        $livre = $userRepository->find(1);
+        dump($livre);
+
+        // Récupération des livres dont le titre contient le mot clé `lorem`
+
+
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);
